421476fc8435818aecd0b54589341533457482d7
spyincorp <theincorporatedspy@gmail.com>
2024-05-18 14:05:29 -0400

    Added changes to block reader to pull more stats.
    Signed-off-by: spyincorp <theincorporatedspy@gmail.com>

--- Data/BlockReader.py
+++ Data/BlockReader.py
@@ -5,46 +5,142 @@ import csv
 import os
 import pandas as pd
 
+
 def beastiary_to_dataframe():
-    
-    subdirectories = ['Library\\pathfinder-bestiary','Library\\pathfinder-bestiary-2','Library\\pathfinder-bestiary-3']
-    
-    data = [['Name', 'Level', 'Traits']]
+
+    subdirectories = [
+        "Library\\pathfinder-bestiary",
+        "Library\\pathfinder-bestiary-2",
+        "Library\\pathfinder-bestiary-3",
+    ]
+
+    data = [
+        [
+            "Name",
+            "Level",
+            "Traits",
+            "AC",
+            "HP",
+            "Speed",
+            "Cha",
+            "Con",
+            "Dex",
+            "Int",
+            "Str",
+            "Wis",
+        ]
+    ]
 
     for sub in subdirectories:
-        files = glob.glob(f'{sub}/*.json', recursive=True)
+        files = glob.glob(f"{sub}/*.json", recursive=True)
 
         for single_file in files:
-            with open(single_file, 'r', encoding='utf-8') as f:
+            with open(single_file, "r", encoding="utf-8") as f:
                 try:
                     json_file = json.load(f)
-                    name = json_file.get("name", "")  
-                    level = json_file.get("system", "").get("details", "").get("level", "").get("value", "")
-                    traits =  json_file.get("system", "").get("traits", "").get("value", "")
-                    if name:  
-                        data.append([name, level, traits]) 
+                    name = json_file.get("name", "")
+                    level = (
+                        json_file.get("system", "")
+                        .get("details", "")
+                        .get("level", "")
+                        .get("value", "")
+                    )
+                    traits = (
+                        json_file.get("system", "").get("traits", "").get("value", "")
+                    )
+                    ac = (
+                        json_file.get("system", "")
+                        .get("attributes", "")
+                        .get("ac", "")
+                        .get("value", "")
+                    )
+                    hp = (
+                        json_file.get("system", "")
+                        .get("attributes", "")
+                        .get("hp", "")
+                        .get("value", "")
+                    )
+                    speed = (
+                        json_file.get("system", "")
+                        .get("attributes", "")
+                        .get("speed", "")
+                        .get("value", "")
+                    )
+                    cha = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("cha", "")
+                        .get("mod", "")
+                    )
+                    con = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("con", "")
+                        .get("mod", "")
+                    )
+                    dex = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("dex", "")
+                        .get("mod", "")
+                    )
+                    int = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("int", "")
+                        .get("mod", "")
+                    )
+                    str = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("str", "")
+                        .get("mod", "")
+                    )
+                    wis = (
+                        json_file.get("system", "")
+                        .get("abilities", "")
+                        .get("wis", "")
+                        .get("mod", "")
+                    )
+
+                    if name:
+                        data.append(
+                            [
+                                name,
+                                level,
+                                traits,
+                                ac,
+                                hp,
+                                speed,
+                                cha,
+                                con,
+                                dex,
+                                int,
+                                str,
+                                wis,
+                            ]
+                        )
                 except json.JSONDecodeError as e:
                     print(f"Error decoding JSON in {single_file}: {e}")
-                    continue  
-
-  
-
+                    continue
 
     df = pd.DataFrame(data[1:], columns=data[0])
-    df['Level'] = pd.to_numeric(df['Level'], errors='coerce')  # Convert 'Level' column to numeric for sorting
-    
+    df["Level"] = pd.to_numeric(
+        df["Level"], errors="coerce"
+    )  # Convert 'Level' column to numeric for sorting
+
     # Sort DataFrame by 'Level' column
-    df_sorted = df.sort_values(by=['Level', 'Name'])
-    
+    df_sorted = df.sort_values(by=["Level", "Name"])
+
     return df_sorted
+
 
 def dataframe_to_csv():
     df = beastiary_to_dataframe()
-    output_dir = os.path.join('Data', 'BeastiaryCsv')
+    output_dir = os.path.join("Data", "BeastiaryCsv")
     os.makedirs(output_dir, exist_ok=True)
-    output_file = os.path.join(output_dir, 'Beastiary.csv')
-    df.to_csv(output_file, index=False)  
-
+    output_file = os.path.join(output_dir, "Beastiary.csv")
+    df.to_csv(output_file, index=False)
 
 
 if __name__ == "__main__":
--- Data/CreatureTools.py
+++ Data/CreatureTools.py
@@ -4,51 +4,85 @@ import pandas as pd
 
 from BlockReader import dataframe_to_csv, beastiary_to_dataframe
 
+data = [
+    "Name",
+    "Level",
+    "Traits",
+    "AC",
+    "HP",
+    "Speed",
+    "Cha",
+    "Con",
+    "Dex",
+    "Int",
+    "Str",
+    "Wis",
+]
+
+
 def print_title_card(instance):
-    name = instance.__class__.__name__ 
-    print('\033[92m' + f'====================={name}=====================' + '\033[0m \n')
+    name = instance.__class__.__name__
+    print(
+        "\033[92m" + f"====================={name}=====================" + "\033[0m \n"
+    )
+
 
 class creature:
-    def __init__(self, cr=0, type=None,  min=None, max=None, name =''):
+    def __init__(self, cr=0, type=None, min=None, max=None, name=""):
         self.cr = cr
         self.type = type
         self.min = min
         self.max = max
         self.name = name
+
     def updatebeastiary():
         dataframe_to_csv()
-      
+
     def getRandomMonster(self):
         print_title_card(self)
         if self.min is None or self.max is None:
             raise ValueError("Minimum and maximum levels must be provided.")
-                   
-        else:
-            df = pd.read_csv('Data\BeastiaryCsv\Beastiary.csv')
-            mask = (df['Level'] >= self.min) & (df['Level'] <= self.max)
-            rangedf =df[mask]
-            rc =rangedf.sample(n=1)
-            out2 = rc[['Name','Level']].to_string(index=False)
 
-            print(out2 + '\n')
+        else:
+            df = pd.read_csv("Data\BeastiaryCsv\Beastiary.csv")
+            mask = (df["Level"] >= self.min) & (df["Level"] <= self.max)
+            rangedf = df[mask]
+            rc = rangedf.sample(n=1)
+            out2 = rc[data].to_string(index=False)
+
+            print(out2 + "\n")
+
+    def getByLevel(self):
+        print_title_card(self)
+        if self.min is None or self.max is None:
+            raise ValueError("Minimum and maximum levels must be provided.")
+
+        else:
+            df = pd.read_csv("Data\BeastiaryCsv\Beastiary.csv")
+            mask = (df["Level"] >= self.min) & (df["Level"] <= self.max)
+            rangedf = df[mask]
+            rc = rangedf.sample(n=1)
+            out2 = rc[data].to_string(index=False)
+
+            print(out2 + "\n")
 
     def getByTrait(self):
-        df = pd.read_csv('Data\BeastiaryCsv\Beastiary.csv')
-        mask = df['Traits'].apply(lambda x: f'{self.type}' in x)
-        traitsdf =df[mask]
-        return(traitsdf)
+        df = pd.read_csv("Data\BeastiaryCsv\Beastiary.csv")
+        mask = df["Traits"].apply(lambda x: f"{self.type}" in x)
+        traitsdf = df[mask]
+        return traitsdf
 
     def __getByNameDf(self):
-        df = pd.read_csv('Data\BeastiaryCsv\Beastiary.csv')
-        mask =df['Name'].str.contains(self.name, case=False)
+        df = pd.read_csv("Data\BeastiaryCsv\Beastiary.csv")
+        mask = df["Name"].str.contains(self.name, case=False)
         namedf = df[mask]
-        out = namedf[['Name','Level']].to_string(index=False)
-        return(namedf)
-    
+        out = namedf[data].to_string(index=False)
+        return namedf
+
     def getByName(self):
         print_title_card(self)
         namedf = self.__getByNameDf()
-        out = namedf[['Name','Level']].to_string(index=False)
+        out = namedf[data].to_string(index=False)
         print(out)
 
     def getNPC(self):
@@ -57,33 +91,33 @@ class creature:
     def getRandomNPC(self):
         pass
 
+
 class npc(creature):
-    def __init__(self, cr=0, type='',  min=None, max=None, name =''):
-        super().__init__(cr, type,  min, max, name)
+    def __init__(self, cr=0, type="", min=None, max=None, name=""):
+        super().__init__(cr, type, min, max, name)
 
     def getNPC(self):
-        self.type = 'humanoid'
+        self.type = "humanoid"
         npcs = self.getByTrait()
-        return(npcs)
-    
+        return npcs
+
     def getRandomNPC(self):
         print_title_card(self)
         df = self.getNPC()
-        mask = (df['Level'] >= self.min) & (df['Level'] <= self.max)
-        rangedf =df[mask]
-        rc =rangedf.sample(n=1)
-        out = rc[['Name','Level']].to_string(index=False)
+        mask = (df["Level"] >= self.min) & (df["Level"] <= self.max)
+        rangedf = df[mask]
+        rc = rangedf.sample(n=1)
+        out = rc[data].to_string(index=False)
 
-        print(out + '\n') 
+        print(out + "\n")
 
-    
+
 if __name__ == "__main__":
-    C1 = creature(min=-1, max = 3)
+    C1 = creature(min=-1, max=3)
     C1.getRandomMonster()
 
-    C2 = npc(min =7 , max =12)
+    C2 = npc(min=7, max=12)
     C2.getRandomNPC()
 
-    C3 = npc(name = 'dragon')
+    C3 = npc(name="dragon")
     C3.getByName()
-  
